e670977a7a89a50b63afdbb4150c59f72f550d42
raphaelstary <raphael.stary@gmail.com>
2015-01-16 23:24:33 +0100

    simple sandbox with just walls ready refs #1 #game

--- www/js/scene/myscenes/PlayGame.js
+++ www/js/scene/myscenes/PlayGame.js
@@ -10,17 +10,27 @@ var PlayGame = (function (window, Event, Math, Key) {
         var startX = 300;
         var startY = 500;
 
-        var rect = this.stage.drawRectangle(startX, startY, 50, 25, '#000');
+        var river = this.stage.drawRectangle(780/2, 1200/2, 780 - 25 - 25 + 25, 1200, '#81BEF7', true, undefined, 0);
+
+        var rect = this.stage.drawRectangle(startX, startY, 50, 25, '#000', false, 1, 3);
         rect.rotation = -Math.PI / 2;
-        var circ = this.stage.drawCircle(rect.x, rect.y, 50, '#000');
-        var line = this.stage.drawLine(rect.x, rect.y, circ.data.radius, '#000');
+        var circ = this.stage.drawCircle(rect.x, rect.y, 30, '#000', false, 1, 4);
+        var line = this.stage.drawLine(rect.x, rect.y, circ.data.radius, '#000', 1, 4);
         line.anchorOffsetX = line.getWidthHalf();
         line.rotation = rect.rotation;
 
+        var waterfall = this.stage.drawRectangle(780/2, 25, 1080, 50, '#5882FA', true, undefined, 1);
+        var leftBanks = this.stage.drawRectangle(25, 1200/2, 50, 1200, '#088A08', true, undefined, 2);
+        var rightBanks = this.stage.drawRectangle(780, 1200/2, 50, 1200, '#088A08', true, undefined, 2);
+        var chaser = this.stage.drawRectangle(780/2, 1080, 1080, 50, '#B45F04', true, undefined, 1);
         var rowBoat = {
             forceX: 0,
             forceY: 0,
-            drawable: rect
+            drawable: rect,
+            direction: line,
+            collision: circ,
+            lastX: 0,
+            lastY: 0
         };
 
         function paddleOnPortSide() { // backbord
@@ -97,6 +107,8 @@ var PlayGame = (function (window, Event, Math, Key) {
             forceX += rowBoat.forceX;
             forceY += rowBoat.forceY;
 
+            rowBoat.lastX = rowBoat.drawable.x;
+            rowBoat.lastY = rowBoat.drawable.y;
             rowBoat.drawable.x += forceX;
             rowBoat.drawable.y += forceY;
 
@@ -108,8 +120,17 @@ var PlayGame = (function (window, Event, Math, Key) {
             line.rotation = rect.rotation;
         });
 
-        function forceStuff() {
-        }
+        var buildings = [waterfall, leftBanks, rightBanks, chaser];
+        this.events.subscribe(Event.TICK_COLLISION, function () {
+            buildings.forEach(function (building) {
+                if (circ.x + circ.data.radius > building.getCornerX() && circ.x - circ.data.radius < building.getEndX() &&
+                    circ.y + circ.data.radius > building.getCornerY() && circ.y - circ.data.radius < building.getEndY()) {
+                    console.log('collision');
+                    rowBoat.drawable.x = rowBoat.lastX;
+                    rowBoat.drawable.y = rowBoat.lastY;
+                }
+            })
+        });
 
     };
 
