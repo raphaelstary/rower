830a24b41688db6bd7e52fcc0e060c9ad4e46a86
raphaelstary <raphael.stary@gmail.com>
2015-01-17 14:23:28 +0100

    refactoring value bindings refs #1 #game

--- www/js/scene/myscenes/PlayGame.js
+++ www/js/scene/myscenes/PlayGame.js
@@ -9,11 +9,12 @@ var PlayGame = (function (window, Event, Math, Key) {
     PlayGame.prototype.show = function (next) {
         var startX = 300;
         var startY = 500;
+        var startRotation = -Math.PI / 2;
 
         var river = this.stage.drawRectangle(780/2, 1200/2, 780 - 25 - 25 + 25, 1200, '#81BEF7', true, undefined, 0);
 
         var rect = this.stage.drawRectangle(startX, startY, 50, 25, '#000', false, 1, 3);
-        rect.rotation = -Math.PI / 2;
+        rect.rotation = startRotation;
         var circ = this.stage.drawCircle(rect.x, rect.y, 30, '#000', false, 1, 4);
         var line = this.stage.drawLine(rect.x, rect.y, circ.data.radius, '#000', 1, 4);
         line.anchorOffsetX = line.getWidthHalf();
@@ -23,14 +24,35 @@ var PlayGame = (function (window, Event, Math, Key) {
         var leftBanks = this.stage.drawRectangle(25, 1200/2, 50, 1200, '#088A08', true, undefined, 2);
         var rightBanks = this.stage.drawRectangle(780, 1200/2, 50, 1200, '#088A08', true, undefined, 2);
         var chaser = this.stage.drawRectangle(780/2, 1080, 1080, 50, '#B45F04', true, undefined, 1);
+
         var rowBoat = {
+            setX: function (x) {
+                this.x = x;
+                this.drawable.x = x;
+                this.collision.x = x;
+                this.direction.x = x;
+            },
+            setY: function (y) {
+                this.y = y;
+                this.drawable.y = y;
+                this.direction.y = y;
+                this.collision.y = y;
+            },
+            setRotation: function (angle) {
+                this.rotation = angle;
+                this.drawable.rotation = angle;
+                this.direction.rotation = angle;
+            },
+            x: startX,
+            y: startY,
+            rotation: startRotation,
             forceX: 0,
             forceY: 0,
             drawable: rect,
             direction: line,
             collision: circ,
-            lastX: 0,
-            lastY: 0
+            lastX: startX,
+            lastY: startY
         };
 
         function paddleOnPortSide() { // backbord
@@ -46,7 +68,7 @@ var PlayGame = (function (window, Event, Math, Key) {
             var magnitude = 10;
             rowBoat.forceX += magnitude * Math.cos(angle);
             rowBoat.forceY += magnitude * Math.sin(angle);
-            rowBoat.drawable.rotation = angle;
+            rowBoat.setRotation(angle);
         }
 
         function toRadians(degrees) {
@@ -107,17 +129,10 @@ var PlayGame = (function (window, Event, Math, Key) {
             forceX += rowBoat.forceX;
             forceY += rowBoat.forceY;
 
-            rowBoat.lastX = rowBoat.drawable.x;
-            rowBoat.lastY = rowBoat.drawable.y;
-            rowBoat.drawable.x += forceX;
-            rowBoat.drawable.y += forceY;
-
-            // sync drawables
-            circ.x = rect.x;
-            circ.y = rect.y;
-            line.x = rect.x;
-            line.y = rect.y;
-            line.rotation = rect.rotation;
+            rowBoat.lastX = rowBoat.x;
+            rowBoat.lastY = rowBoat.y;
+            rowBoat.setX(rowBoat.x + forceX);
+            rowBoat.setY(rowBoat.y + forceY);
         });
 
         var buildings = [waterfall, leftBanks, rightBanks, chaser];
@@ -126,12 +141,12 @@ var PlayGame = (function (window, Event, Math, Key) {
                 if (circ.x + circ.data.radius > building.getCornerX() && circ.x - circ.data.radius < building.getEndX() &&
                     circ.y + circ.data.radius > building.getCornerY() && circ.y - circ.data.radius < building.getEndY()) {
                     console.log('collision');
-                    rowBoat.drawable.x = rowBoat.lastX;
-                    rowBoat.drawable.y = rowBoat.lastY;
-                }
-            })
-        });
 
+                    rowBoat.setX(rowBoat.lastX);
+                    rowBoat.setY(rowBoat.lastY);
+                }
+            });
+        });
     };
 
     return PlayGame;
