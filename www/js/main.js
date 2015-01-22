d3e1c79d617c40065cc7c2687e48b7921e9abdbe
raphaelstary <raphael.stary@gmail.com>
2015-01-22 13:01:55 +0100

    refactoring canvas & pointer refs #6 #game

--- www/js/main.js
+++ www/js/main.js
@@ -1,6 +1,6 @@
 window.onload = function () {
     "use strict";
 
-    var app = Bootstrapper.gamePad().keyBoard().build(MyGameResources);
+    var app = Bootstrapper.gamePad().keyBoard().lowRez(256, 240).build(MyGameResources);
     app.start();
 };
